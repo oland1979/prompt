f2047aceabbff08bd89ef294a370ad53cbe5d46e
oland1979 <oland1979@gmail.com>
2024-06-04 11:49:22 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let myName
+myName = prompt("Як Ваше Ім'я?")
+let mySurname
+mySurname = prompt("Як Ваше прізвище?")
+let myAge
+myAge = prompt("Який Ваш вік?")
+console.log("Ваше ім'я:" + " " + myName + " , " + "Ваше прізвище:" + " " + mySurname + " , " + "Ваш вік:" + " " + myAge)
